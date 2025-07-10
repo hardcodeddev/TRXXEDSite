49c4bf2a34321b3740c24f559c926631dee9f1aa
HardCodedDev <prestonfernando@gmail.com>
2025-07-09 21:38:37 -0400

    Adding logging

--- supabase/supabase.ts
+++ supabase/supabase.ts
@@ -4,4 +4,6 @@ import { Database } from '../types'
 
 export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 export const supabaseAnonKey = import.meta.env.VITE_SUPABASE_API;
+console.log("Supabase URL:", import.meta.env.VITE_SUPABASE_URL);
+console.log("Supabase URL:", import.meta.env.VITE_SUPABASE_API);
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
