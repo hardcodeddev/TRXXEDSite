6b0d26f1f05b8f6ebc2264c63994c9ec77cffe2e
HardCodedDev <prestonfernando@gmail.com>
2025-07-09 21:12:32 -0400

    Changing dist stuff

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react()],
-    base: 'TRXXEDSite',
+    base: '/TRXXEDSite/',
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
